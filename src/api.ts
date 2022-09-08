53c56963b4de23487c1961f10e2d1ab20c2a9497
Nathaniel McLean <nmclean@abledocs.com>
2022-09-08 15:09:11 -0400

    add user to localStorage

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,8 @@ type HttpMethod = "GET" | "POST" | "PUT" | "PATCH";
 
 type AuthorizeRequest = {username: string, password: string, ttl?: number};
 
+type AuthenticatedUser = {username: string, jwt: string};
+
 async function sendJson(path: string, method: HttpMethod, request: object) {
     return await fetch(
         `${process.env.REACT_APP_ADAPI_BASE_URL}/${path}`,
@@ -15,6 +17,13 @@ async function sendJson(path: string, method: HttpMethod, request: object) {
 }
 
 export async function authorize(request: AuthorizeRequest) {
-    return await sendJson("authorize", "POST", request);
+    localStorage.removeItem("user");
+    const response = await sendJson("authorize", "POST", request);
+    if (response.ok) {
+        const jwt = await response.text();
+        const user: AuthenticatedUser = {username: request.username, jwt};
+        localStorage.setItem("user", JSON.stringify(user));
+    }
+    return response;
 }
 
