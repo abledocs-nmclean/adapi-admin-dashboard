c11643da33adc57852445ca4767c3cbd0f5763cd
Nathaniel McLean <nmclean@abledocs.com>
2022-09-14 15:53:41 -0400

    add spinner while query loading

--- src/CompanyList.tsx
+++ src/CompanyList.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { GridComponent, ColumnDirective, ColumnsDirective, Inject, Sort } from '@syncfusion/ej2-react-grids';
+import { createSpinner, showSpinner, hideSpinner } from '@syncfusion/ej2-popups';
 import { useAuthContext, useQueryWithAuth } from './auth-context';
 import { getAllCompanies } from './api';
 import { Company } from './model';
@@ -17,17 +18,38 @@ export default function CompanyList() {
         }
     ));
 
+    let container: HTMLElement;
+
+    const containerRef = useCallback(
+        (c: HTMLElement | null) => {
+            if (c === null) return;
+            container = c;
+            createSpinner({
+                target: container
+            });
+        }, []);
+
+    useEffect(() => {
+        if (companiesQuery.isLoading) {
+            showSpinner(container);
+        } else {
+            hideSpinner(container);
+        }
+    }, [companiesQuery.isLoading]);
+
     return (
         <div>
             <h1>Companies</h1>
-            <GridComponent dataSource={companiesQuery.data} allowSorting={true}>
-                <ColumnsDirective>
-                    <ColumnDirective headerText="Name" field="name" />
-                    <ColumnDirective headerText="Active" width={100} field="isActive" />
-                    <ColumnDirective headerText="Trial" width={100} field="isTrial"  />
-                </ColumnsDirective>
-                <Inject services={[Sort]} />
-            </GridComponent>
+            <div ref={containerRef}>
+                <GridComponent dataSource={companiesQuery.data} allowSorting={true}>
+                    <ColumnsDirective>
+                        <ColumnDirective headerText="Name" field="name" />
+                        <ColumnDirective headerText="Active" width={100} field="isActive" />
+                        <ColumnDirective headerText="Trial" width={100} field="isTrial"  />
+                    </ColumnsDirective>
+                    <Inject services={[Sort]} />
+                </GridComponent>
+            </div>
         </div>
     );
 }
