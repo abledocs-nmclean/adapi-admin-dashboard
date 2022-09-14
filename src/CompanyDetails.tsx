053f08fb10a95779a66ec0ce83e7654aaf49dac8
Nathaniel McLean <nmclean@abledocs.com>
2022-09-14 16:42:47 -0400

    call getCompany in CompanyDetails

--- src/CompanyDetails.tsx
+++ src/CompanyDetails.tsx
@@ -1,17 +1,22 @@
 import { useParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
-import { useQueryWithAuth } from "./auth-context";
+import { useAuthContext, useQueryWithAuth } from "./auth-context";
+import { getCompany } from "./api";
+import { Company } from './model';
 
 type CompanyRouteParams = {id: string};
 
 export default function CompanyDetails() {
     const { id } = useParams<CompanyRouteParams>();
+    
+    const { user } = useAuthContext();
 
     const companyQuery = useQueryWithAuth(useQuery(
         ["company", id],
-        () => ({}),
+        () => getCompany(user!, id!),
         {
-            enabled: id !== undefined
+            enabled: user !== null && id !== undefined,
+            retry: false
         }
     ));
 
