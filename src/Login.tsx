652fc0de1a4049267406456f5a58ee282c8308d7
Nathaniel McLean <nmclean@abledocs.com>
2022-09-19 13:13:48 -0400

    display error on login page

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,6 +4,7 @@ import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { TextBoxComponent } from '@syncfusion/ej2-react-inputs';
 import { ApiError } from './api'
 import { useAuthContext } from './auth-context';
+import { getErrorDisplayMessage } from './util';
 import './Login.css';
 
 export default function Login() {
@@ -16,6 +17,7 @@ export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     const isValid = useMemo(
         () => username.length > 0 && password.length > 0,
@@ -27,12 +29,16 @@ export default function Login() {
     async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         
+        setErrorMessage(null);
         setIsLoading(true);
         try {
             await login(username, password);
         } catch (error) {
-            if (error instanceof ApiError) {
-                // todo
+            if (error instanceof ApiError && error.response.status === 401) {
+                setErrorMessage("Username or password is incorrect");
+            } else {
+                const displayMessage = await getErrorDisplayMessage(error);
+                setErrorMessage(displayMessage);
             }
             return;
         } finally {
@@ -46,15 +52,14 @@ export default function Login() {
         <div className="login-page">
             <form onSubmit={handleLogin}>
                 <h1>Login</h1>
-                <div className="field-container">
-                    <TextBoxComponent placeholder="Username" cssClass="e-outline" floatLabelType="Auto"
-                        value={username} input={({value}) => setUsername(value)} />
-                </div>
-                <div className="field-container">
-                    <TextBoxComponent type="password" placeholder="Password" cssClass="e-outline" floatLabelType="Auto"
-                        value={password} input={({value}) => setPassword(value)} />
-                </div>
+                <TextBoxComponent placeholder="Username" cssClass="e-outline" floatLabelType="Auto"
+                    value={username} input={({value}) => setUsername(value)} />
+                <TextBoxComponent type="password" placeholder="Password" cssClass="e-outline" floatLabelType="Auto"
+                    value={password} input={({value}) => setPassword(value)} />
                 <ButtonComponent type="submit" disabled={isLoading || !isValid} isPrimary={true} cssClass="e-block">Login</ButtonComponent>
+                {errorMessage &&
+                    <div className="error">Couldn't log in: {errorMessage}</div>
+                }
             </form>
         </div>
     );
