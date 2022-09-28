88d1eab636c178a9aff0704ef9e88761e6abad66
Nathaniel McLean <nmclean@abledocs.com>
2022-09-27 20:34:09 -0400

    use class based CompanyEdit component

--- src/CompanyEdit.tsx
+++ src/CompanyEdit.tsx
@@ -1,49 +1,40 @@
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { CheckBoxComponent } from '@syncfusion/ej2-react-buttons';
 import { TextBoxComponent, InputEventArgs } from '@syncfusion/ej2-react-inputs';
 import { Company } from "./model";
 import './CompanyEdit.css'
 
-type CompanyEditProps = {
-    company?: Company,
-    onChange?: (data: Partial<Company>) => void
-};
+export type CompanyEditModel = Partial<Company> & {isAdd?: boolean};
 
-export default function CompanyEdit({onChange, company: existingCompany}: CompanyEditProps) {
-    const [name, setName] = useState(() => existingCompany?.name ?? "");
-    const [adoClientId, setAdoClientId] = useState(existingCompany?.adoClientId);
-    const [isActive, setIsActive] = useState(() => existingCompany?.isActive ?? true);
-    const [isTrial, setIsTrial] = useState(() => existingCompany?.isTrial ?? false);
+export default class CompanyEdit extends React.Component<CompanyEditModel, CompanyEditModel> {
+    constructor(props: CompanyEditModel) {
+        super(props);
+        this.state = {...props, isActive: props.isActive ?? true};
+    }
 
-    const isValid = useMemo(() => {
-        return name.length > 0 && adoClientId !== undefined;
-    }, [name, adoClientId]);
-
-    useEffect(() => {
-        if (onChange) onChange({name, adoClientId, isActive, isTrial});
-    }, [onChange, name, adoClientId, isActive, isTrial]);
-
-    // async function sendChange() {
-    //     if (existingCompany?.id) {
-    //         return await companyEditMutation.mutateAsync({id: existingCompany.id, request: {name, adoClientId, isActive, isTrial}})
-    //     }
-    //     return await companyAddMutation.mutateAsync({name, adoClientId: adoClientId!, isActive, isTrial});
-    // }
-
-    return (
-            <form className="company-edit">
+    public render() {
+        return (
+            <div className="company-edit">
                 <TextBoxComponent placeholder="Name" cssClass="e-outline" floatLabelType="Auto"
-                    value={name} input={({value}) => setName(value)} />
+                    disabled={!this.state.isAdd}
+                    name="name" value={this.state.name}
+                    input={({value}: InputEventArgs) => this.setState({name: value})} />
 
                 <TextBoxComponent type="number" placeholder="ADO Client ID" cssClass="e-outline" floatLabelType="Auto"
-                    value={`${adoClientId ?? ""}`} input={({value}: InputEventArgs) => setAdoClientId(value ? parseInt(value) : undefined)} />
+                    name="adoClientId" value={`${this.state.adoClientId ?? ""}`}
+                    input={({value}: InputEventArgs) => this.setState({adoClientId: value ? parseInt(value) : undefined})} />
 
                 <div className="checkboxes">
                     <CheckBoxComponent label="Active" labelPosition={"Before"}
-                        checked={isActive} change={({checked}) => setIsActive(checked)} />
+                        name="isActive" checked={this.state.isActive}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            this.setState({isActive: e.currentTarget.checked})} />
                     <CheckBoxComponent label="Trial" labelPosition={"Before"}
-                        checked={isTrial} change={({checked}) => setIsTrial(checked)} />
+                        name="isTrial" checked={this.state.isTrial}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            this.setState({isTrial: e.currentTarget.checked})} />
                 </div>
-            </form>
-    );
+            </div>
+        );
+    }
 }
