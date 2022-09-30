7d9a38562a444bf1a555b185007574faa4fed48f
Nathaniel McLean <nmclean@abledocs.com>
2022-09-30 14:30:22 -0400

    allow UserEdit state type as Create/Update request

--- src/dialogs/common.ts
+++ src/dialogs/common.ts
@@ -3,10 +3,11 @@ import { filterUndefined } from "../common";
 
 export type EditModel<TModel> = Partial<TModel> & {isAdd?: boolean};
 
-export abstract class EditForm<TModel> extends React.Component<EditModel<TModel>, EditModel<TModel>> {
-    constructor(props: EditModel<TModel>, defaults: Partial<TModel> = {}) {
+export abstract class EditForm<TModel, TEditState = Partial<TModel>>
+        extends React.Component<EditModel<TModel>, EditModel<TEditState>> {
+    constructor(props: EditModel<TModel>, defaultState: EditModel<TEditState> = {}) {
         super(props);
         // state is initialized with defaults, which are overridden with any input model properties that are not undefined
-        this.state = {...defaults, ...filterUndefined(props)};
+        this.state = {...defaultState, ...filterUndefined(props)};
     }
 }
